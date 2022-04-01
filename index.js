30f91b10ebd06ee260400cee7faf85c3ea33e763
Travis James <travisjamesss@gmail.com>
2022-04-01 18:22:53 -0500

    v1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const parser = require('xml2json')
+// const bodyParser = require('body-parser')
+const xmlparser = require('express-xml-bodyparser')
+const app = express()
+const port = 4000
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(xmlparser())
+
+app.listen(port, () => {
+    console.log("Test API Server Started");
+});
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError) {
+        return res.status(400).send();
+    }
+    next();
+})
+
+app.post("/xmldata/:ser_num/:date", (req, res) => {
+    console.log("POST Request: /xmldata/" + req.params.ser_num + "/" + req.params.date);
+    console.log(req.body)
+
+    let xmldata = req.body
+
+    res.status(200).send({
+        success: true,
+        resss: req.body
+    })
+})
+
+module.exports = app
